b5caecb7e84e9529ea21128b36259fc998825477
jbprojects95 <158042318+jbprojects95@users.noreply.github.com>
2024-04-20 23:07:00 +0100

    Created the bare bones for a very simple rockpaperscissors game that tracks player and computer points

---
+++ rockpaperscissors.js
@@ -0,0 +1,90 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerChoice;
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  return computerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (
+    playerChoice === playerChoice.toUpperCase() ||
+    playerChoice.toLowerCase()
+  ) {
+    playerChoice = playerChoice.toLowerCase();
+  }
+
+  if (playerChoice === computerChoice) {
+    return draws++, "It's a draw!";
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    return computerWin++, "Computer wins! Paper beats rock.";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    return playerWin++, "Player wins! Paper beats rock.";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return playerWin++, "Player wins. Scissors beat paper";
+  } else if (playerChoice === "paper" && computerChoice === "scissors") {
+    return computerWin++, "Computer wins! Scissors beat paper.";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    return playerWin++, "Player wins! Rock beats scissors. ";
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    return computerWin++, "Computer wins! Rock beats scissors. ";
+  }
+}
+
+let playerWin = 0;
+let computerWin = 0;
+let draws = 0;
+
+function playGame() {
+  let playerSelection = prompt("Rock, paper or scissors?");
+  let computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player wins: ${playerWin}`);
+  console.log(`Computer wins: ${computerWin}`);
+  console.log(`Draws: ${draws}`);
+
+  playerSelection = prompt("Rock, paper or scissors?");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player wins: ${playerWin}`);
+  console.log(`Computer wins: ${computerWin}`);
+  console.log(`Draws: ${draws}`);
+
+  playerSelection = prompt("Rock, paper or scissors?");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player wins: ${playerWin}`);
+  console.log(`Computer wins: ${computerWin}`);
+  console.log(`Draws: ${draws}`);
+
+  playerSelection = prompt("Rock, paper or scissors?");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player wins: ${playerWin}`);
+  console.log(`Computer wins: ${computerWin}`);
+  console.log(`Draws: ${draws}`);
+
+  playerSelection = prompt("Rock, paper or scissors?");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Player wins: ${playerWin}`);
+  console.log(`Computer wins: ${computerWin}`);
+  console.log(`Draws: ${draws}`);
+
+  if (playerWin > computerWin) {
+    console.log(`Player wins with ${playerWin} points!`)
+  }
+  else if (computerWin > playerWin) {
+    console.log(`Computer wins with ${computerWin} points!`)
+  } else { console.log(`Player and Computer tied!`)}
+
+  
+}
+
+playGame();
